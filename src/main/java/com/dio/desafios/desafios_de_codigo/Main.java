4715fe6ae67a69c4a152663e4e7b666b16046b10
Isaque Abednego <isaqueabednego2022@gmail.com>
2025-10-29 02:07:40 -0300

    atualizando progresso no curso

--- src/main/java/com/dio/desafios/desafios_de_codigo/Main.java
+++ src/main/java/com/dio/desafios/desafios_de_codigo/Main.java
@@ -8,10 +8,29 @@ public class Main {
 
     public static void main(String[] args) {
 
+        // Posso ter duas variáveis //
+
+        String primeiroNome = "Isaque Abednego";
+
+        String segundoNome = "de Souza Freitas";
+
+        String nomeCompleto = nomeCompleto(primeiroNome, segundoNome);
+        System.out.println(nomeCompleto);
+
+        // Printando algo na tela para o usuário //
+
         System.out.println("Hello World");
         System.out.println("Isaque developer Java Junior in 2026");
 
     }
+    // Declarando um segundo metodo //
+
+    public static String nomeCompleto(String primeiroNome, String segundoNome) {
+        return "Resultado método " + primeiroNome.concat(" ").concat(segundoNome);
+
+
+    }
+
 }
 
 
